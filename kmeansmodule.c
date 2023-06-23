2651f4f856543c7e61a3a0b7bb974f6dc86f4933
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-06-23 16:26:08 +0300

    Create kmeansmodule.c

---
+++ kmeansmodule.c
@@ -0,0 +1,15 @@
+#define PY_SSIZE_T_CLEAN 
+#include <Python.h>
+
+static PyObject* geo_sum(PyObject *self, PyObject *args)
+{
+    int k , iter;
+    /* This parses the Python arguments into a double (d)  variable named z and int (i) variable named n*/
+    if(!PyArg_ParseTuple(args, "ii", &k, &iter)) {
+        return NULL; /* In the CPython API, a NULL value is never valid for a
+                        PyObject* so it is used to signal that an error has occurred. */
+    }
+
+/* This builds the answer ("d" = Convert a C double to a Python floating point number) back into a python object */
+    return Py_BuildValue("d", kmeans_c(z, n)); /*  Py_BuildValue(...) returns a PyObject*  */
+}
