27e27ed73000e97935c28eaee7dc33b69495615e
edenbuky <edenbuky@mail.tau.ac.il>
2023-06-23 21:04:17 +0300

    updteing

--- kmeans.c
+++ kmeans.c
@@ -3,41 +3,7 @@
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
-
-#define MAX_ITER 1000
-#define EPSILON 0.001
-
-typedef struct {
-    double *coordinates;
-} point;
-
-typedef struct {
-    point *points;
-    point centroid;
-    int size;
-} cluster;
-
-point* readPointsFromFile(int* numPoints, int* dimensions);
-
-void initializeClusters(point* points, int K, cluster* clusters, int d);
-
-void findNearestCluster(int* res, point x, cluster* clusters, int numClusters, int d);
-
-double euclideanDistance(double* p, double* q, int d);
-
-void freePoints(point* points, int numPoints);
-
-void freeClusters(cluster* clusters, int K);
-
-void printInvalidInputError(const char* message);
-
-void printPoint(point point, int d);
-
-void updateCentroids(cluster* clusters, int K, int d, int* isSame);
-
-void oneIter(point* points, int N, cluster* clusters, int K, int d, int* isSame);
-
-int isNumber(const char* str);
+#include "kmeans.h"
 
 int main(int argc, char **argv){
     int K, d, N, iter, isCentroidsSame, i;
@@ -317,6 +283,7 @@ void oneIter(point* points, int N, cluster* clusters, int K, int d, int* isSame)
         clusters[i].size = 0;
     }
 }
+
 int isNumber(const char* str) {
     if (str == NULL || *str == '\0') {
         return 0;
--- kmeansmodule.c
+++ kmeansmodule.c
@@ -1,7 +1,37 @@
 #define PY_SSIZE_T_CLEAN 
 #include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include "kmeans.h"
+#include "kmeans.c"
 
-static PyObject* mykmeansssp(PyObject *self, PyObject *args)
+typedef struct _object {
+    Py_ssize_t ob_refcnt;
+    struct _typeobject *ob_type;
+} PyObject;
+
+static PyObject *fit(PyObject *self, PyObject *args);
+
+static PyMethodDef MyKMeansSPMethods[] = {
+    {"fit",
+     fit,
+    METH_VARARGS, "Fit K-means algorithm with initial centroids."},
+    {NULL, NULL, 0, NULL}  /* Sentinel */
+};
+
+PyMODINIT_FUNC PyInit_mykmeanssp(void) 
+{
+    PyObject *m;
+    m = yModule_Create(&mykmeansspmodule);
+    if (m) {
+        return NULL;
+    }
+    return m;
+} 
+static PyObject *fit(PyObject *self, PyObject *args)
 {
     int k , iter;
     /* This parses the Python arguments into a double (d)  variable named z and int (i) variable named n*/
@@ -13,3 +43,12 @@ static PyObject* mykmeansssp(PyObject *self, PyObject *args)
 /* This builds the answer ("d" = Convert a C double to a Python floating point number) back into a python object */
     return Py_BuildValue("d", kmeans_c(z, n)); /*  Py_BuildValue(...) returns a PyObject*  */
 }
+
+static struct PyModuleDef mykmeansspmodule = {
+    PyModuleDef_HEAD_INIT,
+    "mykmeanssp",   /* name of module */
+    NULL,           /* module documentation, may be NULL */
+    -1,             /* size of per-interpreter state of the module,
+                       or -1 if the module keeps state in global variables. */
+    MyKMeansSPMethods
+};
