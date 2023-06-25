b3089b9517df715d3004034c7f94aa8f0537fa70
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-06-25 18:32:40 +0300

    Update kmeans.h

--- kmeans.h
+++ kmeans.h
@@ -1,7 +1,4 @@
-
 #define MAX_ITER 1000
-#define EPSILON 0.001
-
 typedef struct {
     double *coordinates;
 } point;
@@ -12,24 +9,16 @@ typedef struct {
     int size;
 } cluster;
 
-point* readPointsFromFile(int* numPoints, int* dimensions);
-
-void initializeClusters(point* points, int K, cluster* clusters, int d);
-
 void findNearestCluster(int* res, point x, cluster* clusters, int numClusters, int d);
 
 double euclideanDistance(double* p, double* q, int d);
 
 void freePoints(point* points, int numPoints);
 
-void freeClusters(cluster* clusters, int K);
-
 void printInvalidInputError(const char* message);
 
 void printPoint(point point, int d);
 
-void updateCentroids(cluster* clusters, int K, int d, int* isSame);
+void updateCentroids(cluster* clusters, int K, int d, int* isSame, double epsilon);
 
-void oneIter(point* points, int N, cluster* clusters, int K, int d, int* isSame);
-
-int isNumber(const char* str);
+void oneIter(point* points, int N, cluster* clusters, int K, int d, int* isSame, double eps);
