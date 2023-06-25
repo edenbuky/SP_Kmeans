b267a7a36a5cc2dacf173656c661a401925918fb
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-06-25 13:34:20 +0300

    Update kmeansmodule.c

--- kmeansmodule.c
+++ kmeansmodule.c
@@ -17,15 +17,22 @@ static PyObject *fit(PyObject *self, PyObject *args);
 
 static PyMethodDef MyKMeansSPMethods[] = {
     {"fit",
-     fit,
-    METH_VARARGS, "Fit K-means algorithm with initial centroids."},
+     (PyCFunction) fit,
+    METH_VARARGS,
+    PyDoc_STR("Fit K-means algorithm with initial centroids.")},
     {NULL, NULL, 0, NULL}  /* Sentinel */
 };
-
+static struct PyModuleDef mykmeansspmodule = {
+        PyModuleDef_HEAD_INIT,
+        "mykmeanssp",
+        NULL,
+        -1,
+        MyKMeansSPMethods
+};
 PyMODINIT_FUNC PyInit_mykmeanssp(void) 
 {
     PyObject *m;
-    m = yModule_Create(&mykmeansspmodule);
+    m = PyModule_Create(&mykmeansspmodule);
     if (m) {
         return NULL;
     }
@@ -43,12 +50,3 @@ static PyObject *fit(PyObject *self, PyObject *args)
 /* This builds the answer ("d" = Convert a C double to a Python floating point number) back into a python object */
     return Py_BuildValue("d", kmeans_c(z, n)); /*  Py_BuildValue(...) returns a PyObject*  */
 }
-
-static struct PyModuleDef mykmeansspmodule = {
-    PyModuleDef_HEAD_INIT,
-    "mykmeanssp",   /* name of module */
-    NULL,           /* module documentation, may be NULL */
-    -1,             /* size of per-interpreter state of the module,
-                       or -1 if the module keeps state in global variables. */
-    MyKMeansSPMethods
-};
